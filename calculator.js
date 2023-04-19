1d611823daf0ae4f234782d9df0268f1745f98e3
rahulsharma7504 <rahul658541@gmail.com>
2023-04-19 15:32:04 +0530

    initial commit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+
+let screen = document.getElementById('text');
+let show = document.getElementById('on');
+buttons = document.querySelectorAll('button')
+var screenvalue = ''
+
+function helo() {
+  var clear = screen.value = '  '
+
+}
+document.getElementById('target').addEventListener('click', net);
+function net() {
+
+  document.getElementById('text').value = eval(text.value)
+
+}
+
